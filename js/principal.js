746334f8c53008b7408afdbdf5541da9ca355ff5
winicius-leal <winiciusleal@hotmail.com>
2021-09-04 16:37:09 -0300

    Calculando IMC e exibindo

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,20 @@
 /*alterando conteudo do HTML com o DOM*/
 
-//var titulo = document.querySelector("h1");
 var titulo = document.querySelector(".title");
 titulo.textContent = "WM SUPLEMENTOS";
+
+
+var TrPrimeiroPaciente = document.querySelector("#primary-row");
+
+var TdPeso = TrPrimeiroPaciente.querySelector(".info-peso");
+var peso = TdPeso.textContent;
+
+var TdAltura = TrPrimeiroPaciente.querySelector(".info-altura");
+var altura = TdAltura.textContent;
+
+var imc = peso / (altura * altura);
+
+var TdImc = TrPrimeiroPaciente.querySelector(".info-imc");
+TdImc.textContent = imc;
+
+
